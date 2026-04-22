78f4304801f5106a75917332edd15aabec7d3a60
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-22 19:40:44 +0600

    update Vacancy

--- src/main/java/kg/attractor/lesson55lab/model/Vacancy.java
+++ src/main/java/kg/attractor/lesson55lab/model/Vacancy.java
@@ -14,6 +14,14 @@ public class Vacancy {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
     private String title;
     private String description;
     private Double salary;
