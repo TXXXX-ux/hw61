078808074085629b7079c2ccfddace84404ea725
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-22 19:40:03 +0600

    update User

--- src/main/java/kg/attractor/lesson55lab/model/User.java
+++ src/main/java/kg/attractor/lesson55lab/model/User.java
@@ -2,6 +2,7 @@ package kg.attractor.lesson55lab.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.util.List;
 
 @Data
 @Builder
@@ -17,7 +18,23 @@ public class User {
     private String password;
     private String name;
     private String about;
+
     @Enumerated(EnumType.STRING)
     private AccountType accountType;
+
     private String avatar;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id")
+    )
+    private List<Authority> authorities;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Vacancy> vacancies;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Resume> resumes;
 }
