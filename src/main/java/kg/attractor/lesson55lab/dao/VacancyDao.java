611137a84a5622251c2a96c4fd4231f78625558e
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-22 19:35:11 +0600

    update VacancyDao

--- src/main/java/kg/attractor/lesson55lab/dao/VacancyDao.java
+++ src/main/java/kg/attractor/lesson55lab/dao/VacancyDao.java
@@ -1,7 +1,11 @@
 package kg.attractor.lesson55lab.dao;
 
+import kg.attractor.lesson55lab.model.User;
 import kg.attractor.lesson55lab.model.Vacancy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VacancyDao extends JpaRepository<Vacancy, Long> {
+    Page<Vacancy> findByAuthor(User author, Pageable pageable);
 }
