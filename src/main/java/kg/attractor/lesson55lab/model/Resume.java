5c66ff787640710bec712f0d6150e79d73311884
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-22 19:39:39 +0600

    update Resume

--- src/main/java/kg/attractor/lesson55lab/model/Resume.java
+++ src/main/java/kg/attractor/lesson55lab/model/Resume.java
@@ -3,6 +3,7 @@ package kg.attractor.lesson55lab.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+
 @Entity
 @Table(name = "resumes")
 @Data
@@ -12,6 +13,15 @@ public class Resume {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
     private String title;
     private String description;
 }
