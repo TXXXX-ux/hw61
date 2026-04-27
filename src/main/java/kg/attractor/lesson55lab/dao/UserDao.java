d4a95e17b1cd4cb7c65edb07a80d8816aed28a49
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-27 15:04:24 +0600

    update UserDao

--- src/main/java/kg/attractor/lesson55lab/dao/UserDao.java
+++ src/main/java/kg/attractor/lesson55lab/dao/UserDao.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserDao extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     Page<User> findByAccountType(AccountType accountType, Pageable pageable);
+    Optional<User> findByResetPasswordToken(String token);
 }
