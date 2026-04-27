04e1da8ce755b141278082156d68f147a7af9be9
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-27 18:28:34 +0600

    update GlobalControllerAdvice

--- src/main/java/kg/attractor/lesson55lab/handler/GlobalControllerAdvice.java
+++ src/main/java/kg/attractor/lesson55lab/handler/GlobalControllerAdvice.java
@@ -12,25 +12,22 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-    // Перехватываем ошибку 403 (Нет прав доступа) от Spring Security
     @ExceptionHandler(AccessDeniedException.class)
     public String handleAccessDeniedException(AccessDeniedException ex, Model model, HttpServletRequest request) {
 
-        // Передаем данные, которые ждет твой шаблон error.ftlh
         model.addAttribute("status", "403");
         model.addAttribute("reason", "Доступ запрещен! У вас нет прав для просмотра этой страницы.");
 
-        // Передаем URL, на который пытался зайти юзер
         Map<String, String> details = new HashMap<>();
         details.put("requestURL", request.getRequestURL().toString());
         model.addAttribute("details", details);
 
-        return "error"; // Вызываем твою фирменную страницу
+        return "error";
     }
 
     @ExceptionHandler(Exception.class)
     public String handleGlobalException(Exception ex, Model model) {
         model.addAttribute("error", "Произошла непредвиденная ошибка: " + ex.getMessage());
-        return "error"; // Должен существовать шаблон error.ftlh
+        return "error";
     }
 }
